913dd1e4fadc5a56d3b26cb4516b009fff4ebd66
Angry Coding <mailidanielifechukwu@gmail.com>
2023-09-06 02:40:04 +0000

    feat:implementing the ast tree

---
+++ lamport-go/ast/ast.go
@@ -0,0 +1,42 @@
+package ast
+
+import (
+	"lamort-go/token"
+)
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+type Program struct {
+	Statement []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statement) > 0 {
+		return p.Statement[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
